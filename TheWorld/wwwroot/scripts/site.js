9b37b1a51c265aa1bc15b149385611206b873a3d
Zachary Kirshbaum <zachary.kirshbaum@slalom.com>
2017-07-10 16:37:58 -0700

    controllers etc

---
+++ TheWorld/wwwroot/scripts/site.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var sidebarAndWrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").on("click", function () {
+        sidebarAndWrapper.toggleClass("hide-sidebar");
+        if (sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Menu");
+        } else {
+            $(this).text("Hide Menu");
+        }
+    });
+
+    
+})();
